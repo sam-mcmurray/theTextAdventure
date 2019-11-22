9d8be1e85c7446f8d2ef6b4ccb8e2f8e450187a4
Samuel Mcmurray <samuel_joseph.mcmurray0004@stud.hkr.se>
2019-11-22 11:28:50 +0100

    added some methods but just the name

--- src/project1E7/TheTextAdventure.java
+++ src/project1E7/TheTextAdventure.java
@@ -4,4 +4,17 @@ public class TheTextAdventure {
     public static void main(String[] args) {
     TheTextAdventure myApp = new TheTextAdventure();
     }
+    private void createWorld() {
+
+    }
+    private void createHeroes() {
+
+    }
+    private void printStartMenu() {
+
+    }
+    private void printHeroes() {
+
+    }
+    
 }
