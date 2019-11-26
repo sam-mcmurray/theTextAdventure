ccb6e2b35277fdfac690f7f6f0d27c674f9b551b
OussamaMhSafi <oussamamhsafi@gmail.com>
2019-11-26 13:44:54 +0100

    added the encounter menu

--- src/project1E7/TheTextAdventure.java
+++ src/project1E7/TheTextAdventure.java
@@ -1,20 +1,34 @@
 package project1E7;
 
+import project1E7.Model.Hero;
+
+import java.util.*;
+import java.security.*;
 public class TheTextAdventure {
+
+
     public static void main(String[] args) {
-    TheTextAdventure myApp = new TheTextAdventure();
+        TheTextAdventure myApp = new TheTextAdventure();
     }
+
+
     private void createWorld() {
 
     }
+
     private void createHeroes() {
 
+        //run Wael's code
     }
+
     private void printStartMenu() {
 
     }
+
     private void printHeroes() {
 
     }
+
+
 
 }
