9f126b11690ca596b0fa2fa7663807e9165ec947
Samuel Mcmurray <samuel_joseph.mcmurray0004@stud.hkr.se>
2019-11-28 00:10:07 +0100

    deleted the useless .idea files updated git ignore to exclude them

--- src/project1E7/TheTextAdventure.java
+++ src/project1E7/TheTextAdventure.java
@@ -13,9 +13,9 @@ public class TheTextAdventure {
 
     public static void main(String[] args) {
         TheTextAdventure myApp = new TheTextAdventure();
-
         HeroView hero = new HeroView(myApp.selectHero());
         hero.printStats();
+
     }
 
     public Hero selectHero() {
