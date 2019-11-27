bde94f7f58111758a15a78b6b24a02ea42a58733
Lukas <57799079+OlssonLukas@users.noreply.github.com>
2019-11-27 20:00:10 +0100

    flavor class

---
+++ src/project1E7/Controller/FlavorTextController.java
@@ -0,0 +1,43 @@
+package src.project1E7.Controller;
+
+public class FlavorTextController {
+
+    private String introFlavorText;
+
+    private String roomFlavorText1;
+    private String roomFlavorText2;
+    private String roomFlavorText3;
+    private String roomFlavorText4;
+    private String roomFlavorText5;
+
+    private String encounterFlavorTextSpiderling1;
+    private String encounterFlavorTextSpiderling2;
+    private String encounterFlavorTextBat1;
+    private String encounterFlavorTextBat2;
+    private String encounterFlavorTextSlime1;
+    private String encounterFlavorTextSlime2;
+    private String encounterFlavorTextSkeleton1;
+    private String encounterFlavorTextSkeleton2;
+    private String encounterFlavorTextOwlbear1;
+    private String encounterFlavorTextOwlbear2;
+    private String encounterFlavorTextBoss;
+
+    private String attackFlavorText1;
+    private String attackFlavorText2;
+    private String missFlavorText1;
+    private String missFlavorText2;
+
+    private String bossWarning;
+    private String bossFlavorText;
+
+    private String keyWoodenFlavorText;
+    private String doorWoodenFlavorText;
+    private String keyStoneFlavorText;
+    private String doorStoneFlavorText;
+    private String keySilverFlavorText;
+    private String doorSilverFlavorText;
+    private String keyGoldFlavorText;
+    private String doorGoldFlavorText;
+
+    
+}
